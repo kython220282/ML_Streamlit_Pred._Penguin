747606e7758ca4bb577bcfb6d5582b4eb40e30e3
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2024-08-13 21:14:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit as st
+import pandas as pd
+
 st.set_page_config(layout="wide")
 
 st.title('Machine Learning Application')
 st.info('This application builds a Machine learning model!')
-st.write('By: Karan Raj sharma')
+st.write('By: Karan Raj Sharma')
 st.write('--------')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
