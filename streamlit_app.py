008844182c1797dd57dc454a586cd4be383f4c44
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2024-08-13 20:26:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ st.set_page_config(layout="wide")
 
 st.title('Machine Learning Application')
 st.info('This application builds a Machine learning model!')
-st.write('Karan Raj sharma')
+st.write('By: Karan Raj sharma')
 st.write('--------')
