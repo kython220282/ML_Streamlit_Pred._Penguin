e62fe726d68b1264df91ceb81024e13fd96517dc
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2024-08-13 20:24:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,6 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 st.title('Machine Learning Application')
+st.info('This application builds a Machine learning model!')
 st.write('By Karan Raj Sharma')
 st.write('--------')
