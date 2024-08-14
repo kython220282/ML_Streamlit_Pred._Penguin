56ed3e31938a6c55fbe88bdf2cda9a6d1085f21b
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2024-08-14 20:12:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,4 +52,7 @@ with st.expander('Input features'):
   st.write('**Combined Penguins data**')
   input_penguins
   
-  
+#Encode Categorical Variables
+encode = ['island','sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+df_penguins
