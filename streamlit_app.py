46cf1be00a7f80364194f2253913a99ee12a0c92
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2024-08-14 21:29:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,6 +86,13 @@ clf.fit(X,y)
 prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
 
+df_prediction_proba = pd.DataFrame(prediction_proba)
+df_prediction_proba.columns = ['Adelie','Chinstrap','Gentoo']
+prediction_proba.rename(columns={0:'Adelie',
+                                 1:'Chinstrap'
+                                 2:'Gentoo'})
+
+
 prediction_proba
 
 
