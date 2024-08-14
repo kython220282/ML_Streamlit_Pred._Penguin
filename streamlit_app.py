a454f99fa0e0540423049ee6ed30c94098bd6a38
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2024-08-14 21:09:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,7 +84,7 @@ clf.fit(X,y)
 
 #Apply model to make prediction
 prediction = clf.predict(input_row)
-prediction_proba = clf.predict_prob(input_row)
+prediction_proba = clf.predict_proba(input_row)
 
 prediction_proba
 
