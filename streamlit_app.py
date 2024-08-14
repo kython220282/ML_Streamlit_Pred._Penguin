f0485bccc2a6fee2ce980bb8fc3ecc667e93722a
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2024-08-14 06:31:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,10 @@ with st.expander('Data'):
 
 with st.expander('Data Visualisation'):
   st.scatter_chart(data = df, x = 'bill_depth_mm', y = 'bill_length_mm', color='species')
+  st.scatter_chart(data = df, x = 'bill_depth_mm', y = 'bill_length_mm', color='island')
 
-
+# Data Prepration
+with st.slider:
+  #island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
+  #island = st.selectbox('Island',
 
